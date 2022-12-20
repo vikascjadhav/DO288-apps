fe69cc7307a5e6c48e906ca11302ac8ee9c6cb85
Student User <student@workstation.lab.example.com>
2022-12-20 08:39:42 -0500

    examp practice

---
+++ exam-EX288-practice/template/quote.sql
@@ -0,0 +1,4 @@
+CREATE TABLE quote(id int,msg varchar(255));
+INSERT INTO quote VALUES (1,"This is Quote1 from Vikas Jadhav- Be true to yourself");
+INSERT INTO quote VALUES (2,"This is Quote2 from Vikas Jadhav- Be Confident");
+INSERT INTO quote VALUES (3,"This is Quote2 from Vikas Jadhav- Believe in yourself");
