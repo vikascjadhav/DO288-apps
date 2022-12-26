e50e02bf3f4886a88b1630149c34b1400dff7807
Student User <student@workstation.lab.example.com>
2022-12-26 06:44:54 -0500

    practice day2

--- exam-practice/optimize/with-scc-added/run.sh
+++ exam-practice/optimize/with-scc-added/run.sh
@@ -3,11 +3,12 @@ podman rm build
 echo " cleanup complete and runnign new container"
 printf "\n"
 #podman run -d --name build localhost/build:latest 
-podman build -t build . --format=docker
+podman build -t build . --format=docker --layers=false
 podman tag localhost/build quay.io/vikacjadhav/build
 podman push quay.io/vikacjadhav/build
+oc import-image build --from quay.io/vikacjadhav/build --confirm
 oc delete all -l app=build
-oc new-app --name build quay.io/vikacjadhav/build 
+oc new-app --name  build -i build
 oc delete route build
 oc expose svc build
 sleep 10
